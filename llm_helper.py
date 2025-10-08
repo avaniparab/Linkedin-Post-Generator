700cb7cde2bc4e6d8d72f06b8c54a8c0f127ef6e
avaniparab <avaniparab007@gmail.com>
2025-10-08 22:08:54 +0530

    prompt-template

--- llm_helper.py
+++ llm_helper.py
@@ -1,8 +1,8 @@
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 import os
 
-load_dotenv(find_dotenv())
+load_dotenv()
 
 llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name= "llama-3.1-8b-instant")
 
--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,8 @@
 import json
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from llm_helper import llm
 
 def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"):
     enriched_posts = []
@@ -13,11 +17,27 @@ def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"
             print(epost)
 
 def extract_metadata(post):
-    return {
-        'line_count': 10,
-        'language': 'English',
-        'tags': ['Mental Health', 'Motivation']
-    }
+    template = '''
+    You are given a LinkedIn post. You need to extract number of lines, language of the post and tags.
+    1. Return a valid JSON. No preamble. 
+    2. JSON object should have exactly three keys: line_count, language and tags. 
+    3. tags is an array of text tags. Extract maximum two tags.
+    4. Language should be English or Hinglish (Hinglish means hindi + english)
+    
+    Here is the actual post on which you need to perform this task:  
+    {post}
+    '''
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+    response = chain.invoke(input={'post': post})
+
+    try:
+        json_parser = JsonOutputParser()
+        res = json_parser.parse(response.content)
+    except OutputParserException:
+        raise OutputParserException("Context too big. Unable to parse the response.")
+    return res
+
 
 if __name__ == "__main__":
     process_posts("data/raw_posts.json", "data/processed_posts.json")
